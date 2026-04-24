ea95e11bc7dce7ce0ac5cf079e42317d4b10a663
zeeshan7611 <72546761+zeeshan7611@users.noreply.github.com>
2026-04-24 11:57:35 +0530

    Update PropertyUser.ts

--- src/models/PropertyUser.ts
+++ src/models/PropertyUser.ts
@@ -31,6 +31,7 @@ const PropertyUserSchema: Schema = new Schema({
   designation: { type: String },
   joiningDate: { type: Date },
   monthlySalary: { type: Number },
+  passwordHash : {type:String}
   kycDocument: {
     adharCard: { type: String },
     panCard: { type: String },
